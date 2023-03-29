fabc71fa9b7f7830a0d93679930522190b9e1f3a
Federico Caballero <98574058+fede-caballero@users.noreply.github.com>
2023-03-28 23:19:26 -0300

    Ejercicio funcionando correctamente

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -1,27 +1,37 @@
-chain = input("Ingresa tu expresion: ")
+expr1 = str(input("Enter the expression to solve: "))
 
+def solve(expr):
+    terms = expr.split("+")
+    addends = []
+    multiples = []
+    mult_sep = []
+    prod_tot = []
+    sum_list = []
+    
+    for term in terms:
+        if term.isdigit():
+            int_term = int(term)
+            addends.append(int_term)
+        else:
+            multiples.append(term)
+    
+    if len(multiples) == 1:
+        sep_mult = mult.split("*")
+        mult_sep.append(sep_mult)
+    
+    elif len(multiples) > 1:
+        for mult in multiples:
+            groups = [int(x) for x in mult.split("*")]
+            mult_sep.append(groups)
+    
+    for sub in mult_sep:
+        product = sub[0] * sub[1]
+        prod_tot.append(product)
+        
+    sum_list = addends + prod_tot
+    
+    total = sum(sum_list)
+    
+    print(f"El resultado a tu expresion es: {total}")
 
-def solve(chain):
-    list_mult = []
-    spl_1 = chain.split("+")
-    for i in spl_1:
-        spl_2 = i.split("*")
-        list_mult.append(spl_2)
-    num_sum = 0
-    result = 0
-    num_mult = 0
-    for terms in list_mult:
-        result += num_mult
-        num_mult = 1
-        for mult in terms:
-            if len(terms) != 1:
-                num_mult *= int(mult)
-            elif len(terms) == 1:
-                num_sum += int(mult)
-                result += num_sum
-                num_mult = 0
-    result += num_mult
-    print("El resultado es: ", int(result))
-
-
-solve(chain)
+solve(expr1)
