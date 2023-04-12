f807542576f6e052eeb2b64f750ac4fb27651d78
Federico Caballero <98574058+fede-caballero@users.noreply.github.com>
2023-04-12 20:49:29 -0300

    Ejercicio 2 funcionando sin problemas

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -1,37 +1,36 @@
-expr1 = str(input("Enter the expression to solve: "))
+expr = str(input("Ingresa la expresion a resolver: "))
 
 def solve(expr):
     terms = expr.split("+")
     addends = []
-    multiples = []
-    mult_sep = []
-    prod_tot = []
-    sum_list = []
+    mult_raw = []
+    mult_pre_results = []
+    final_list = []
     
+    #Recorro la expresion y separo los terminos con el simbolo "+"
     for term in terms:
         if term.isdigit():
             int_term = int(term)
             addends.append(int_term)
+                    
         else:
-            multiples.append(term)
+            mult_raw.append(term)
     
-    if len(multiples) == 1:
+    #Recorro la lista de los elementos que se estan multiplicando y separo por el simbolo "*"
+    for mult in mult_raw:
         sep_mult = mult.split("*")
-        mult_sep.append(sep_mult)
-    
-    elif len(multiples) > 1:
-        for mult in multiples:
-            groups = [int(x) for x in mult.split("*")]
-            mult_sep.append(groups)
-    
-    for sub in mult_sep:
-        product = sub[0] * sub[1]
-        prod_tot.append(product)
+        num1 = int(sep_mult[0])
+        num2 = int(sep_mult[1])
         
-    sum_list = addends + prod_tot
+        result = num1 * num2
+    #Agrego a la lista los resultados de las multiplicaciones que estan en la expresion
+        mult_pre_results.append(result)
+        
+    #Sumo todo
+    final_list = addends + mult_pre_results
     
-    total = sum(sum_list)
+    total = sum(final_list)
     
-    print(f"El resultado a tu expresion es: {total}")
-
-solve(expr1)
+    print(f"El resultado de tu expresion es {total}")
+ 
+solve(expr)
