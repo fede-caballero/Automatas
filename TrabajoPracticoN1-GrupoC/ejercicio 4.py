e8a943ecc1ec9caf6dbb0cfe9524e6558ca998a7
Federico Caballero <98574058+fede-caballero@users.noreply.github.com>
2023-05-07 18:07:08 -0300

    Ejercicio 4

---
+++ TrabajoPracticoN1-GrupoC/ejercicio 4.py
@@ -0,0 +1,31 @@
+import re
+
+def analyze_text(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        text = f.read()
+
+    text_without_punctuation = re.sub(r"[^\w\s]", "", text)
+    unique_words = []
+    words = text_without_punctuation.lower().split()
+    total_words = len(words)
+
+    for word in words:
+        if word not in unique_words:
+            unique_words.append(word)
+
+    max_count = 0
+    most_common_word = ""
+
+    for word in unique_words:
+        count = words.count(word)
+        if count > max_count:
+            max_count = count
+            most_common_word = word
+    print(f"The amount of words is: {total_words}")
+    return most_common_word, max_count
+
+
+file_path = "./Archivos_Texto/texto.txt"
+most_common_word, count = analyze_text(file_path)
+
+print(f"The most common word is '{most_common_word}' with {count} occurrences.")
